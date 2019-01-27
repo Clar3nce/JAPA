dfdd6fcaeb62bc88e1a6ce74d2ad1885372352f9
Clar3nce <clar3nce@protonmail.ch>
2019-01-27 18:22:59 +0000

    modified:   src/core/comms_discord.py 	tidying of how commands are parsed and messages received 	finished chat clear command !cmd clear <length>

--- src/core/comms_discord.py
+++ src/core/comms_discord.py
@@ -4,39 +4,34 @@ import asyncio
 
 TOKEN = "NTM5MDk5OTQ2ODU1NTYzMjY1.Dy9btA.AZQP6DyHrqmWm6ndv9Yt-zrIAts"
 
-MSG_HISTORY = []
-
-
 
 def init_client():
     client = discord.Client()
     return client
-japa_client_discord = discord.Client()
 
-def log_message(msgLog,message):
-    msgLog.append(message)
-
-@japa_client_discord.event
-async def clearChat(channel):
-    async for message in japa_client_discord.logs_from(channel,limit=100):
-        await japa_client_discord.delete_message(message)
-        print(message.content)
+japa_client_discord = init_client()
 
 
 
 def isCommand(message):
     commands = ["clear","test"]
-    if message == commands[0]:
-        #do commands function
-        response = "clear"
+    for i in range(len(commands)):
+        if message == commands[i]:
+            return commands[i]
+        else:
+            i += 1
+    return "invalid"
 
-    elif message == commands[1]:
-        response = "test"
 
-    else:
-        response = "[#]Invalid Command"
 
-    return response
+
+
+@japa_client_discord.event
+async def clearChat(channel, length):
+    async for message in japa_client_discord.logs_from(channel,limit=int(length)+3):
+        await japa_client_discord.delete_message(message)
+
+
 
 
 
@@ -45,22 +40,18 @@ def isCommand(message):
 
 @japa_client_discord.event
 async def on_message(message):
-    if message.content != None:
-        log_message(MSG_HISTORY,message)
-    msg_parts = message.content.split(" ")
     if message.author == japa_client_discord.user:
         return
     if message.content.startswith("!cmd"):
-        msg = "Received command"
-        await japa_client_discord.send_message(message.channel, msg)
-        msg = isCommand(msg_parts[1])
-        if msg == "[#]Invalid Command":
+        msg_parts = message.content.split(" ")
+        command = isCommand(msg_parts[1])
+        if command == "invalid":
             msg = "Invalid Command {0.author.mention}".format(message)
             await japa_client_discord.send_message(message.channel, msg)
-        elif msg == "clear":
+        elif command == "clear":
             msg = "Clearing Chat {0.author.mention}".format(message)
             await japa_client_discord.send_message(message.channel, msg)
-            await clearChat(message.channel)
+            await clearChat(message.channel,msg_parts[2])
 
 
 
