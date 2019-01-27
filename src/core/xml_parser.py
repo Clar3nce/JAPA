91733e0bc13f54eef02fbae667623bcee9e0ef4f
Clar3nce <clar3nce@protonmail.ch>
2019-01-27 21:55:43 +0000

    modified:   configuration/config.xml 	modified:   core/xml_parser.py
    	xml_parser successfully parsed all attributes of config.xml into the CONFIG class to be later used by the main client to gain information regarding the command interface to use as well as local server information and and bot_tokens

--- src/core/xml_parser.py
+++ src/core/xml_parser.py
@@ -1,5 +1,44 @@
 import xml.etree.ElementTree as ET
 
-tree = ET.parse("../configuration/config.xml")
-root = ET.fromstring("config")
-print(root.tag)
+class CONFIG:
+
+    local_server_ip = ""
+    local_server_port = 6666
+    remote_interface = ""
+    remote_interface_token = ""
+    command_identifier = ""
+    priv_level = 2
+
+    def __init__(self):
+        self.data = []
+
+def load_Default_Config(configPath):
+    c = CONFIG()
+
+    config = ET.parse(configPath)
+    config_root = config.getroot()
+
+
+    c.local_server_ip = config_root.find("./local_serv/local_ip").text
+    c.local_server_port = config_root.find("./local_serv/local_port").text
+
+    c.remote_interface = config_root.find("./remote_interface").attrib
+    c.remote_interface_token = config_root.find("./remote_interface/bot_token").text
+    c.command_identifier = config_root.find("./remote_interface/command_identifier").text
+
+    c.priv_level = config_root.find("./security/priv_level").text
+
+    return c
+
+
+
+
+default_config_path = "configuration/config.xml"
+
+config = load_Default_Config(default_config_path)
+
+print(config.remote_interface_token)
+print(config.command_identifier)
+
+
+
